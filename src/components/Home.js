2c58aa5246262e97b2f6847a48b1ca6c3857e98b
victor kamau <vikakamau04@gmail.com>
2024-04-23 15:52:53 +0300

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
+const divstyle={
+  color: 'firebrick'
+};
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={divstyle}>{name} is a Web Developer from {city}</h1>
+    
+  </div>;
 }
 
 export default Home;
